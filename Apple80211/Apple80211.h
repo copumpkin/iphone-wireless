79cfa2cade8813c47ca2955a715102b1f5b617ef
Daniel Peebles <copumpkin@users.noreply.github.com>
2007-09-01 06:50:06 +0000

    Added check for Objective C in the header file to avoid incompatible pointer warnings

--- Apple80211/Apple80211.h
+++ Apple80211/Apple80211.h
@@ -1,9 +1,32 @@
-#include <CoreFoundation/CoreFoundation.h>
+#ifndef APPLE80211_H_
+#define APPLE80211_H_
 
-int Apple80211Open(void **handle);
+#ifdef __OBJC__
 
-int Apple80211BindToInterface(void *handle, CFStringRef interface);
+    #import <Foundation/NSString.h>
+    #import <Foundation/NSArray.h>
+    #import <Foundation/NSDictionary.h>
 
-int Apple80211Scan(void *handle, CFArrayRef *networks, CFDictionaryRef parameters);
+    int Apple80211Open(void **handle);
 
-int Apple80211Close(void *handle);
+    int Apple80211BindToInterface(void *handle, NSString *interface);
+
+    int Apple80211Scan(void *handle, NSArray **networks, NSDictionary *parameters);
+
+    int Apple80211Close(void *handle);
+
+#else
+
+    #include <CoreFoundation/CoreFoundation.h>
+
+    int Apple80211Open(void **handle);
+
+    int Apple80211BindToInterface(void *handle, CFStringRef interface);
+
+    int Apple80211Scan(void *handle, CFArrayRef *networks, CFDictionaryRef parameters);
+
+    int Apple80211Close(void *handle);
+
+#endif /* __OBJC__ */
+
+#endif /* APPLE80211_H_ */
