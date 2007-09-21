98aa9d28e817e035ec2c20eae78b7a92d6519d52
pradeepta <pradeepta@ec841d0f-9239-0410-bde3-1f89ecb53ed7>
2007-09-21 15:10:01 +0000

    Added code comments

--- CellStumbler/main.c
+++ CellStumbler/main.c
@@ -3,60 +3,82 @@
 #include <CoreFoundation/CoreFoundation.h>
 #include <sys/time.h>
 
+/*
+	main.c : Hack to CoreTelephony. Most of the peculiarities of the code below is based on obervation. 
+*/
+
+/*
+	For some reason, all these variables have to be global. Toolchain bug?
+*/
 CFMachPortRef port;
 struct CTServerConnection *sc=NULL;
 struct CellInfo cellinfo;
-int b,t1;
+int b;
+int t1;
 
-void hexdump(unsigned char * data, int hlen)
-{
-	int t;
-	for(t=0;t<hlen;t++)
-	{
-//		if(t!=0&&t%16==0) printf("\n");
-		printf("%2.2X ", data[t]);
-	}
-	printf("\n");
-}
-
+/* 
+	Never called.
+*/
 void callback()
 {
 	printf("Callback called\n");
 }
 
+/*
+ 	Again, _CTServerConnectionCreate() has to be called outside the main(), or whatever is using CTServerConnection. 
+	Hence the existence of this function.
+*/
+	
 void cellconnect()
 {
 	int t1;
 	sc=_CTServerConnectionCreate(kCFAllocatorDefault, callback, NULL);
+
+	/*
+		port is not currently used, shuld be usable with a runloop.
+	*/
 	port=CFMachPortCreateWithPort(kCFAllocatorDefault, _CTServerConnectionGetPort(sc), NULL, NULL, NULL);
+
 	_CTServerConnectionCellMonitorStart(&t1,sc);
+
 	printf("Connected\n");
 }
 
+void getCellInfo()
+{
+	int cellcount;
+	_CTServerConnectionCellMonitorGetCellCount(&t1,sc,&cellcount);
+
+	printf("Cell count: %x\n",cellcount);
+
+	printf("Size = %x\n", sizeof(struct CellInfo));
+
+	unsigned char *a=malloc(sizeof(struct CellInfo));
+
+	for(b=0;b<cellcount;b++)
+	{	
+		//OMG the toolchain is broken, &cellinfo doesn't work
+		_CTServerConnectionCellMonitorGetCellInfo(&t1,sc,b,a); memcpy(&cellinfo,a,sizeof(struct CellInfo));
+		//OMG the toolchain is more broken, these printfs don't work on one line
+		printf("Cell Site: %d, MNC: %d, ",b,cellinfo.servingmnc);
+		printf("Location: %d, Cell ID: %d, Station: %d, ",cellinfo.location, cellinfo.cellid, cellinfo.station);
+		printf("Freq: %d, RxLevel: %d, ", cellinfo.freq, cellinfo.rxlevel);
+		printf("C1: %d, C2: %d\n", cellinfo.c1, cellinfo.c2);
+	}
+	if(a) free(a);
+
+}
 
 int main()
 {
 	printf("iPhone Cell Stumbler\n");
-	int cellcount;
 	cellconnect();
-while(1)
-{
-	_CTServerConnectionCellMonitorGetCellCount(&t1,sc,&cellcount);
-	printf("Cell count: %x\n",cellcount);
-	//cellinfo=malloc(0x24);
-	unsigned char *a=malloc(0x24);
-for(b=0;b<cellcount;b++)
-{	
-//OMG the toolchain is broken, &cellinfo doesn't work
-	_CTServerConnectionCellMonitorGetCellInfo(&t1,sc,b,a); memcpy(&cellinfo,a,0x24);
-//OMG the toolchain is more broken, these printfs don't work on one line
-	printf("Cell Site: %d, MNC: %d, ",b,cellinfo.servingmnc);
-	printf("Location: %d, Cell ID: %d, Station: %d, ",cellinfo.location, cellinfo.cellid, cellinfo.station);
-	printf("Freq: %d, RxLevel: %d, ", cellinfo.freq, cellinfo.rxlevel);
-	printf("C1: %d, C2: %d\n", cellinfo.c1, cellinfo.c2);
-}
-sleep(1);
-}
+	int cellcount;
+	while(1)
+	{
+		getCellInfo();
+		sleep(1);
+	}
 	return 0;
 }
 
