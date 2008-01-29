c3e31ad3f6aafcc4146abbdeaafd53926357d5ce
lokkju <lokkju@ec841d0f-9239-0410-bde3-1f89ecb53ed7>
2008-01-29 20:47:56 +0000

    Added licensing comment block to files, added GPLv3 license file.

--- trunk/Stumbler/MSNetworksManager.h
+++ trunk/Stumbler/MSNetworksManager.h
@@ -1,3 +1,28 @@
+/*******************************************************************************
+ * iPhone-Wireless Project : Stumbler                                          *
+ * Copyright (C) 2007-2008 Pumpkin <pumpkingod@gmail.com>                      *
+ * Copyright (C) 2007-2008 Lokkju <lokkju@gmail.com>                           *
+ *******************************************************************************
+ * $LastChangedDate$ *
+ * $LastChangedBy$ *
+ * $LastChangedRevision$ *
+ * $Id$ *
+ *******************************************************************************
+ *  This program is free software: you can redistribute it and/or modify       *
+ *  it under the terms of the GNU General Public License as published by       *
+ *  the Free Software Foundation, either version 3 of the License, or          *
+ *  (at your option) any later version.                                        *
+ *                                                                             *
+ *  This program is distributed in the hope that it will be useful,            *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ *  GNU General Public License for more details.                               *
+ *                                                                             *
+ *  You should have received a copy of the GNU General Public License          *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.      *
+ *******************************************************************************/
+
+/* $HeadURL$ */
 #import <CoreFoundation/CoreFoundation.h>
 #import <Foundation/NSTimer.h>
 #import <Foundation/Foundation.h>
